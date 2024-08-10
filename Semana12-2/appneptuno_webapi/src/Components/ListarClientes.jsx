e1c7cb4115965e37af3e46c5a11e962cc5492ddb
Gustavo Valera <gvaleramendoza@gmail.com>
2024-08-10 15:49:39 -0500

    Agregados final clase 09/08/24

--- Semana12-2/appneptuno_webapi/src/Components/ListarClientes.jsx
+++ Semana12-2/appneptuno_webapi/src/Components/ListarClientes.jsx
@@ -7,7 +7,18 @@ const ListarClientes = () => {
 
     const[lista,setLista]= useState([]);
 
+    //Ej de hacer consulta por codigo y año (1):
+    const[codigo,setCodigo]=useState("");
+    const[anio,setAnio]=useState(0);
+
     const traerClientes=async()=>{
+
+        //Ej de hacer consulta por codigo y año (2):
+        const ruta2="https://localhost:7141/api/NeptunoAPI/GetPedidosClienteAnio/"+codigo+"/"+anio;
+
+        //Ej(2) back ticks = ALT+96=
+        const ruta3=`https://localhost:7141/api/NeptunoAPI/GetPedidosClienteAnio/${codigo}/${anio}`;
+
         const ruta = await fetch("https://localhost:7141/api/NeptunoAPI/GetClientes");
         const datos= await ruta.json();
         setLista(datos);
